c86d67a905b86130b912b2106f169664f0b0b9f7
AtlantaEmrys2002 <milli@Millicents-MacBook-Air.local>
2025-03-31 12:08:35 +0100

    Updating results.sh

--- project/results.sh
+++ project/results.sh
@@ -1,4 +1,8 @@
-# USE TO SAY OVERVIEW OF RESULTS
+# USE TO GENERATE OVERVIEW OF RESULTS
+echo "RESULTS OVERVIEW"
+echo "Building table..."
 
 # Format results
 python ./analysis/build_results_table.py
+
+echo "Table completed and stored in results_overview.csv"
