d12258736f7cbe32ca205007ae299c60623409da
AtlantaEmrys2002 <milli@Millicents-MacBook-Air.local>
2025-04-03 16:02:51 +0100

    Adding in visualisation function - will attempt to tidy/format visualisation better.

--- project/analysis/visualisation.py
+++ project/analysis/visualisation.py
@@ -9,7 +9,6 @@ import numpy as np
 import os
 import plotly.graph_objects as go
 from skyfield.api import EarthSatellite, load
-import s3dlib.surface as s3d
 
 # Global Timescale (used for determining how to calculate time with Skyfield functions)
 ts = load.timescale()
@@ -81,96 +80,19 @@ def snapshot_time_stamp(time_stamp):
 
 
 def generate_Earth():
+    d = np.pi / 32
 
     radius_of_earth = 6378.135
 
-    u = np.linspace(0, 2 * np.pi, 100)
-    v = np.linspace(0, np.pi, 100)
+    theta, phi = np.mgrid[0:np.pi + d:d, 0:2 * np.pi:d]
 
-    x = radius_of_earth * np.outer(np.cos(u), np.sin(v))
-    y = radius_of_earth * np.outer(np.sin(u), np.sin(v))
-    z = radius_of_earth * np.outer(np.ones(np.size(u)), np.cos(v))
+    # Convert to Cartesian coordinates
+    x = radius_of_earth * np.sin(theta) * np.cos(phi)
+    y = radius_of_earth * np.sin(theta) * np.sin(phi)
+    z = radius_of_earth * np.cos(theta)
+    points = np.vstack([x.ravel(), y.ravel(), z.ravel()])
 
-    return x, y, z
-
-
-def visualise_static(location, tle_file, num_snapshot=94, snapshot_interval=60):
-
-    # INPUTS #
-
-    # Check if any ISL topology exists
-    if os.path.isfile(location + "/isls_0.txt") is False:
-        raise ValueError("At least one ISL topology must have been built in order to calculate a network's link churn.")
-
-    # Read in topology built for given snapshot
-    isls = np.loadtxt(location + "/isls_0.txt").astype(int).T
-
-    # CREATE FIGURE #
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-
-    # MODEL EARTH
-
-    # radius_of_earth = 6378.135
-    #
-    # start_theta = 0
-    # rez = 6
-    # lightDirection = [1, 0.8, 1]
-
-    # earth = s3d.SphericalSurface(rez)
-    # earth.domain(radius_of_earth)
-
-    # Generate coordinates
-    x, y, z = generate_Earth()
-
-    # Plot surface
-    ax.plot_surface(x, y, z, antialiased=False, alpha=1)
-    # mlab.mesh(x, y, z)
-
-    # fig = plt.figure(figsize=plt.figaspect(1), facecolor='k')
-    # ax = fig.add_subplot(111, projection='3d', facecolor='k', aspect='equal')
-    #
-    # ax.add_collection3d(earth)
-    # s3d.auto_scale(ax, earth)
-
-    # GET SATELLITE DESCRIPTIONS #
-    # Get TLEs-formatted data
-    tles_data = read_file(tle_file)
-
-    # Convert TLES data to Skyfield EarthSatellite objects - used to convert satellite positions to geocentric
-    # coordinates - all measurements in km
-    earth_satellite_objects = [EarthSatellite(satellite_i[1], satellite_i[2], satellite_i[0], ts) for satellite_i in
-                               tles_data]
-
-    # TIMES #
-
-    # Calculate times at which satellite positions observed
-    snapshot_times = [snapshot_time_stamp(snapshot_interval * k) for k in range(num_snapshot)]
-
-    for k in snapshot_times:
-
-        # Positions of satellites at each snapshot time
-        sats = np.asarray([i.at(k).position.km for i in earth_satellite_objects])
-
-        satellite_xs = sats.T[0].tolist()
-        satellite_ys = sats.T[1].tolist()
-        satellite_zs = sats.T[2].tolist()
-
-        ax.scatter(satellite_xs, satellite_ys, satellite_zs, marker='o', s=2, color='red')
-
-        # mlab.points3d(satellite_xs, satellite_ys, satellite_zs, scale_factor=0.05)
-
-        # for point in range(len(sats)):
-        #     ax.plot(satellite_xs[point], satellite_ys[point], satellite_zs[point], marker='o', markersize=2,
-        #             color='red')
-        #     # p = s3d.SphericalSurface(rez)
-        #     # p.transform(translate=point)
-        #     # ax.add_collection3d(p)
-
-        break
-
-    plt.show()
-    # mlab.show()
+    return points
 
 
 def visualise_static_plotly(location, tle_file, num_snapshot=94, snapshot_interval=60, constellation_name="Kuiper-630"):
@@ -190,38 +112,18 @@ def visualise_static_plotly(location, tle_file, num_snapshot=94, snapshot_interv
 
     # MODEL EARTH
 
-    # Generate coordinates
     x, y, z = generate_Earth()
 
-    # Plot surface
-    # ax.plot_surface(x, y, z, antialiased=False, alpha=1)
+    fig = go.Figure(data=[go.Scatter3d(x=[], y=[], z=[], marker=dict(color="red", size=2)),
+                          go.Mesh3d(x=x, y=y, z=z, color='lightblue', opacity=1.0, alphahull=0, name="Earth")])
 
-    d = np.pi / 32
+    # fig.add_traces = go.Scatter3d(x=[], y=[], z=[])
 
-    radius_of_earth = 6378.135
-
-    theta, phi = np.mgrid[0:np.pi + d:d, 0:2 * np.pi:d]
-    # Convert to Cartesian coordinates
-    x = radius_of_earth * np.sin(theta) * np.cos(phi)
-    y = radius_of_earth * np.sin(theta) * np.sin(phi)
-    z = radius_of_earth * np.cos(theta)
-    # print(x.shape, y.shape, z.shape)  # (33, 64) (33, 64) (33, 64)
-    points = np.vstack([x.ravel(), y.ravel(), z.ravel()])
-    # print(points.shape)  # (3, 2112)
-    x, y, z = points
-    # print(x.shape, y.shape, z.shape)  # (2112,) (2112,) (2112,)
-
-    # fig = go.Figure()
-    #
-    fig = go.Figure(data=[go.Scatter3d(x=[], y=[], z=[], mode='markers', marker=dict(color="red", size=2)),
-                          go.Mesh3d(x=x, y=y, z=z, color='lightblue', opacity=1.0, alphahull=0)])
-
-    # earth = go.Mesh3d(x=x, y=y, z=z, color='lightblue', opacity=1.0, alphahull=0, name="Earth")
-
-
-    # fig.update_traces(lighting=dict(ambient=0.5, specular=1.0))
+    # Format figure
+    fig.update_layout(paper_bgcolor="black", title_font_color="white")
 
     # GET SATELLITE DESCRIPTIONS #
+
     # Get TLEs-formatted data
     tles_data = read_file(tle_file)
 
@@ -229,10 +131,10 @@ def visualise_static_plotly(location, tle_file, num_snapshot=94, snapshot_interv
     # coordinates - all measurements in km
     earth_satellite_objects = [EarthSatellite(satellite_i[1], satellite_i[2], satellite_i[0], ts) for satellite_i in
                                tles_data]
-    #
-    # # TIMES #
-    #
-    # # Calculate times at which satellite positions observed
+
+    # TIMES #
+
+    # Calculate times at which satellite positions observed
     snapshot_times = [snapshot_time_stamp(snapshot_interval * k) for k in range(num_snapshot)]
 
     frames = []
@@ -240,45 +142,45 @@ def visualise_static_plotly(location, tle_file, num_snapshot=94, snapshot_interv
     for k in snapshot_times:
 
         # Positions of satellites at each snapshot time
-        sats = np.asarray([i.at(k).position.km for i in earth_satellite_objects])
+        sats = np.asarray([i.at(k).position.km for i in earth_satellite_objects]).T.tolist()
 
-        satellite_xs = sats.T[0].tolist()
-        satellite_ys = sats.T[1].tolist()
-        satellite_zs = sats.T[2].tolist()
+        # satellites = go.Scatter3d(x=sats[0], y=sats[1], z=sats[2], mode='markers',
+        #                           name=constellation_name)
+        #
+        # satellites.update(marker_size=2)
 
-        # ax.scatter(satellite_xs, satellite_ys, satellite_zs, marker='o', s=2, color='red')
+        sats = np.asarray(sats).T
 
-        # fig = go.Figure(data=[go.Scatter3d(x=satellite_xs, y=satellite_ys, z=satellite_zs, mode='markers')])
+        # Calculate the isl positions
+        isl_a = [sats[int(isls[0, link])].tolist() for link in range(len(isls[0]))]
+        isl_b = [sats[int(isls[1, link])].tolist() for link in range(len(isls[0]))]
 
-        # fig.add_trace(go.Figure(go.Scatter3d(x=satellite_xs, y=satellite_ys, z=satellite_zs, mode='markers')))
+        links = []
 
-        satellites = go.Scatter3d(x=satellite_xs, y=satellite_ys, z=satellite_zs, mode='markers',
-                                  name=constellation_name)
+        for x in range(len(isls[0])):
+            links.append(isl_a[x])
+            links.append(isl_b[x])
+            links.append([None, None, None])
 
-        satellites.update(marker_size=2)
+        links = links[:-1]
 
+        links = np.asarray(links).T
+
+        satellites = go.Scatter3d(x=links[0], y=links[1], z=links[2], line=dict(color='red'), name=constellation_name)
+
+        # satellites.update(marker_size=2)
+
+        # frames.append(go.Frame(data=[satellites], traces=[0], name=f'frame{k}'))
         frames.append(go.Frame(data=[satellites], traces=[0], name=f'frame{k}'))
-
-        # fig.update_traces(marker_size=2)
-
-        # fig = go.Figure
-
-        # break
-    #
-    # fig = go.Figure(data=[earth, satellites])
-    # fig = go.Figure()
 
     fig.update(frames=frames)
 
-    fig.update_layout(title=constellation_name, updatemenus=[dict(type="buttons",
-                                        buttons=[dict(label="Play",
-                                                      method="animate",
-                                                      args=[None])])])
+    fig.update_layout(title=constellation_name, updatemenus=[dict(type="buttons", buttons=[dict(label="Play",
+                                                                                                method="animate",
+                                                                                                args=[None])])],
+                      scene=dict(xaxis=dict(visible=False), yaxis=dict(visible=False), zaxis=dict(visible=False)))
 
     fig.show()
-
-    # plt.show()
-    # # mlab.show()
 
 
 visualise_static_plotly("./Results/plus_grid/kuiper-630", "kuiper-constellation_tles.txt.tmp", 94, 60, "Kuiper-630")
